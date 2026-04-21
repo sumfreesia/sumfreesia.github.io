4bd10277008ce861f6f304bd18341503bde7ff0f
sumfreesia <bc1d12@gmail.com>
2026-04-22 03:56:36 +0800

    sw.js

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE = "checkin-v1";
+
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE).then(c=>c.addAll([
+      "/",
+      "/index.html",
+      "/icon.png"
+    ]))
+  );
+});
+
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>res || fetch(e.request))
+  );
+});
